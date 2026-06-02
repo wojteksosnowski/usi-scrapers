5431495d4ce466494d3f7d2a61c01af8ce01bda6
wojteksosnowski <wojtek.sosnowski@gmail.com>
2026-06-02 12:43:37 +0200

    chore(release): bump version to 0.8.3

--- usi_scrapers/__init__.py
+++ usi_scrapers/__init__.py
@@ -3,7 +3,7 @@ USI Scrapers Package
 """
 import logging
 
-__version__ = "0.8.2"
+__version__ = "0.8.3"
 
 class USILoggerAdapter(logging.LoggerAdapter):
     """Adds version information to every log message."""
